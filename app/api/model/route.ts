4fd190fac203e350bfac78692c71823142eee7b6
Shanu Sharma <shanu5544@gmail.com>
2025-02-07 12:03:28 +0530

    feat(api/model): create first chain using prompts and parsers.

--- app/api/model/route.ts
+++ app/api/model/route.ts
@@ -1,11 +1,16 @@
 import { Ollama } from "@langchain/ollama";
-// import {StringOutputParser} from '@langchain/core/output_parsers'
+import { StringOutputParser } from '@langchain/core/output_parsers'
+import { RunnableSequence, } from "@langchain/core/runnables";
+import { PromptTemplate } from "@langchain/core/prompts";
 
-export async function GET(request:Request) {
+export async function POST(request:Request) {
+        const {topic} = await request.json();
+        const template = PromptTemplate.fromTemplate("Tell me a joke on {topic}")
+        const model = new Ollama({ model: "deepseek-r1:1.5b" });
+        const parser = new StringOutputParser();
+        const chain =  RunnableSequence.from([template,model,parser])
+        const response: string = await chain.invoke({topic});
 
-        const model = new Ollama({ model: "deepseek-r1:7b" });
-      
-        const response: string = await model.invoke("Name PM of India?");
-
-        return Response.json(response)
+        // could handle using structured output parsers
+        return Response.json(response.replace(/<think>.*?<\/think>/gs,"").trim())
 }
