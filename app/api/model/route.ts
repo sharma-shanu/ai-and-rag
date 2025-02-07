88f5c91e9c9dcb2aee2ac28dd8b3eb4cc4ae4ebf
Shanu Sharma <shanu5544@gmail.com>
2025-02-07 12:41:28 +0530

    chore(/chain): format the files and remove eslint rule for unused vars

--- app/api/model/route.ts
+++ app/api/model/route.ts
@@ -1,16 +1,19 @@
-import { Ollama } from "@langchain/ollama";
-import { StringOutputParser } from '@langchain/core/output_parsers'
-import { RunnableSequence, } from "@langchain/core/runnables";
+import { Ollama, OllamaEmbeddings } from "@langchain/ollama";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { RunnableSequence } from "@langchain/core/runnables";
 import { PromptTemplate } from "@langchain/core/prompts";
 
-export async function POST(request:Request) {
-        const {topic} = await request.json();
-        const template = PromptTemplate.fromTemplate("Tell me a joke on {topic}")
-        const model = new Ollama({ model: "deepseek-r1:1.5b" });
-        const parser = new StringOutputParser();
-        const chain =  RunnableSequence.from([template,model,parser])
-        const response: string = await chain.invoke({topic});
+const model_name = "deepseek-r1:1.5b";
 
-        // could handle using structured output parsers
-        return Response.json(response.replace(/<think>.*?<\/think>/gs,"").trim())
+export async function POST(request: Request) {
+  const { topic } = await request.json();
+  const template = PromptTemplate.fromTemplate("Tell me a joke on {topic}");
+  const model = new Ollama({ model: model_name });
+  const embeddings = new OllamaEmbeddings({ model: model_name });
+  const parser = new StringOutputParser();
+  const chain = RunnableSequence.from([template, model, parser]);
+  const response: string = await chain.invoke({ topic });
+
+  // could handle using structured output parsers
+  return Response.json(response.replace(/<think>.*?<\/think>/gs, "").trim());
 }
